72509372ccb42fdbbd868df9aa0a545fbd54e8aa
Face51 <142801225+Face51@users.noreply.github.com>
2025-09-01 00:27:46 +0530

    Update size.c

--- college/size.c
+++ college/size.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+
 int main() {
-    // printing the sizes of all primitive datatypes in C:
-    printf("Size of all Primitive Datatypes in C:");
-    printf("\nSize of short int = %zu bytes", sizeof(short int));
-    printf("\nSize of unsigned short int = %zu bytes", sizeof(unsigned short int));
-    printf("\nSize of int = %zu bytes", sizeof(int));
-    printf("\nSize of unsigned int = %zu bytes", sizeof(unsigned int));
-    printf("\nSize of long int = %zu bytes", sizeof(long int));
-    printf("\nSize of unsigned long int = %zu bytes", sizeof(unsigned long int));
-    printf("\nSize of long long int = %zu bytes", sizeof(long long int));
-    printf("\nSize of unsigned long long int = %zu bytes", sizeof(unsigned long long int));
-    printf("\nSize of char = %zu byte", sizeof(char));
-    printf("\nSize of signed char = %zu byte", sizeof(signed char));
-    printf("\nSize of unsigned char = %zu byte", sizeof(unsigned char));
-    printf("\nSize of float = %zu bytes", sizeof(float));
-    printf("\nSize of double = %zu bytes", sizeof(double));
-    printf("\nSize of long double = %zu bytes", sizeof(long double));
+    printf("Size of all Primitive Datatypes in C:\n");
+
+    printf("Size of short int = %d bytes\n", (int)sizeof(short int));
+    printf("Size of unsigned short int = %d bytes\n", (int)sizeof(unsigned short int));
+    printf("Size of int = %d bytes\n", (int)sizeof(int));
+    printf("Size of unsigned int = %d bytes\n", (int)sizeof(unsigned int));
+    printf("Size of long int = %d bytes\n", (int)sizeof(long int));
+    printf("Size of unsigned long int = %d bytes\n", (int)sizeof(unsigned long int));
+    printf("Size of long long int = %d bytes\n", (int)sizeof(long long int));
+    printf("Size of unsigned long long int = %d bytes\n", (int)sizeof(unsigned long long int));
+    
+    printf("Size of char = %d byte\n", (int)sizeof(char));
+    printf("Size of signed char = %d byte\n", (int)sizeof(signed char));
+    printf("Size of unsigned char = %d byte\n", (int)sizeof(unsigned char));
+    
+    printf("Size of float = %d bytes\n", (int)sizeof(float));
+    printf("Size of double = %d bytes\n", (int)sizeof(double));
+    printf("Size of long double = %d bytes\n", (int)sizeof(long double));
+
     return 0;
 }
