21a568bfa0bd6a67c5baee0f2527ab71262856b0
Face51 <142801225+Face51@users.noreply.github.com>
2025-09-10 08:55:59 +0530

    Update 8 centered pyramid.c

--- Assingment-2/8 centered pyramid.c
+++ Assingment-2/8 centered pyramid.c
@@ -9,17 +9,23 @@ int main() {
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
+    // Check if input is positive
+    if (n <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1; // Exit if input is invalid
+    }
+
     // Loop for each row
     for(i = 1; i <= n; i++) {
         // Print spaces before stars
         for(j = 1; j <= n - i; j++) {
             printf(" ");
         }
-        // Print stars (2*i - 1 stars in each row)
-        for(j = 1; j <= 2*i - 1; j++) {
+        // Print stars: 2*i - 1 stars in each row
+        for(j = 1; j <= 2 * i - 1; j++) {
             printf("*");
         }
-        printf("\n"); // Move to next row
+        printf("\n"); // Move to the next row
     }
 
     return 0;
